a95e1687053a56cb76d308785061749321c410a8
Swat <swattu@gmail.com>
2018-01-25 18:30:22 +0530

    Emitting information

--- src/test/java/com/plummb/example/ExpensiveServiceTest.java
+++ src/test/java/com/plummb/example/ExpensiveServiceTest.java
@@ -18,10 +18,10 @@ public class ExpensiveServiceTest {
   public void check() throws InterruptedException {
     Setup.initContext();
     ExpensiveService service = new ExpensiveService();
+    System.out.println("The time taken will be emitted in the logs starting with ML (Method Logging)");
     service.delay(25);
-    //The time taken will be emitted in the logs starting with ML (Method Logging)
 
+    System.out.println("The time taken will be emitted in the logs starting with PML (Pending Method Logging). After 60 seconds, this will be emitted every ~10 seconds.");
     service.delay(90000);
-    //The time taken will be emitted in the logs starting with PML (Pending Method Logging). After 60 seconds, this will be emitted every ~10 seconds.
   }
 }
