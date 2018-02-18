6a4e3cb6eff2e62c3f0405930b30eac02a1de5e4
Swat <swattu@gmail.com>
2018-02-18 17:54:31 +0530

    MDC variables retention

--- src/main/java/com/plummb/example/Setup.java
+++ src/main/java/com/plummb/example/Setup.java
@@ -5,11 +5,15 @@
 package com.plummb.example;
 
 import com.plummb.resile.client.ResilienceClient;
+import org.slf4j.MDC;
+
+import java.util.UUID;
 
 public class Setup {
   private static boolean initialized = false;
 
   public static TheContext initContext() throws InterruptedException {
+    MDC.put("requestId", UUID.randomUUID().toString());
     TheContext context = new TheContext();
     context.setSimulationEnabled(true);
     context.setResilienceEnabled(true);
